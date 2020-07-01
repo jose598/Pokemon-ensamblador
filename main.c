ceb5e080503733cf26d42ac76cbd8944f30f23fd
ltvargas <ltvargas@espol.edu.ec>
2020-06-30 21:27:04 -0500

    validacion entre 1 y 4

--- main.c
+++ main.c
@@ -2,11 +2,29 @@
 #include <stdio.h>
 #include <time.h>
 
+//valida si es un num
+int valNum(int n){
+     if(isdigit(n)) 
+     	return 1;
+     else
+	return 0;
+}
+//valida num entre 1 y 4
+int valOpcion(int n){
+    if(n<1 || n>4)
+	return 0;
+     else
+	return 1;
+}
+
 int main(int argc, char **argv){
 
 	FILE *fp=stdin;
 	char buf[10000];
 	char *file="pokeTypes.txt";
+	char ingreso1[]="\nIngrese el número del primer Pokémon para el combate: ";
+	char ingreso2[]="\nIngrese el número del segundo Pokémon para el combate: ";
+	char error[]="\nError,por favor ingrese un número válido(1 al 4): ";
 	int nramdon,nPoke1,nPoke2;
 	int cont=1;
 	int num=0;
@@ -45,8 +63,17 @@ while(!feof(fp) && num<10){
 	}
 
 }
-//printf("\nIngrese el número del primer Pokémon para el combate: ");
-//scanf("%d",&nPoke1);
-//printf("\nIngrese el número del segundo Pokémon para el combate: ");
-//scanf("%d",&nPoke2);
+printf("%s",ingreso1);
+scanf("%d",&nPoke1);
+while((valOpcion(nPoke1)==0) && (valNum(nPoke1)==0)){
+	printf("%s",error);
+	scanf("%d",&nPoke1);
+}
+printf("%s",ingreso2);
+scanf("%d",&nPoke2);
+
+while((valOpcion(nPoke2)==0) && (valNum(nPoke2)==0)){
+	printf("%s",error);
+	scanf("%d",&nPoke2);
+}
 }
