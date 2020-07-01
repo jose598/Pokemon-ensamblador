056b18b4ab88deee0995314fa308694e6039cf78
jose598 <morales2010mendoza@hotmail.com>
2020-06-30 21:49:22 -0500

    Funciones

--- main.c
+++ main.c
@@ -2,19 +2,39 @@
 #include <stdio.h>
 #include <time.h>
 
-//valida si es un num
-int valNum(int n){
-     if(isdigit(n)) 
-     	return 1;
-     else
-	return 0;
-}
 //valida num entre 1 y 4
 int valOpcion(int n){
     if(n<1 || n>4)
 	return 0;
      else
 	return 1;
+}
+int valNum(int num){
+	if(scanf("%d",&num)==0){
+		for (int c=getchar(); c!=EOF && c!=' ' && c!='\n'; c=getchar());
+		return 0;	
+	}
+	return 1;
+}
+
+char* lectura (int nramdon){
+	FILE *fp=stdin;
+	char buf[10000];
+	char *file="pokeTypes.txt";
+	int cont=1;
+	int num=0;
+	fp=fopen(file,"r");
+	while(!feof(fp) && num<10){
+	if((fscanf(fp,"%s",buf)>0)){
+			if(cont==nramdon){
+			printf("%s\n",buf);
+			num++;
+			}else if (num>=1){ printf("%s\n",buf); num++;}
+		cont++;
+		}
+
+	}
+
 }
 
 int main(int argc, char **argv){
@@ -52,17 +72,8 @@ printf("Bienvenido al sistema de combates Pokémon: \n");
 srand(time(NULL));
 nramdon = rand()%(99-1+1) + 1;
 printf("ramdon: %d\n",nramdon);
-fp=fopen(file,"r");
-while(!feof(fp) && num<10){
-	if((fscanf(fp,"%s",buf)>0)){
-			if(cont==nramdon){
-			printf("%s\n",buf);
-			num++;
-			}else if (num>=1){ printf("%s\n",buf); num++;}
-		cont++;
-	}
+lectura(nramdon);
 
-}
 printf("%s",ingreso1);
 scanf("%d",&nPoke1);
 while((valOpcion(nPoke1)==0) && (valNum(nPoke1)==0)){
