431fee68d01d72f513ca1d34652e0a52152b6816
ltvargas <ltvargas@espol.edu.ec>
2020-07-20 00:51:28 -0500

    union de arreglo y matriz, indices tipos

--- Funciones/arreglo.asm
+++ Funciones/arreglo.asm
@@ -17,29 +17,29 @@ tipo15:      .asciiz "ice"
 tipo16:      .asciiz "dragon"
 tipo17:      .asciiz "dark"
 tipo18:      .asciiz "fairy"
-matrizA: .word tipo1,tipo2,tipo3,tipo4,tipo5,tipo6,tipo7,tipo8,tipo9,tipo10,tipo11,tipo12,tipo13,tipo14,tipo15,tipo16,tipo17,tipo18
-	#matrizA: .word 2,3,3,2
+Pokeskin: .word tipo1,tipo2,tipo3,tipo4,tipo5,tipo6,tipo7,tipo8,tipo9,tipo10,tipo11,tipo12,tipo13,tipo14,tipo15,tipo16,tipo17,tipo18
 	nroColumnas: .word 18
 	.eqv longitudDato 4
 	
 espacio: .asciiz " "
 salto: .asciiz "\n"
-poke:  .asciiz "fire"
+poke1:  .asciiz "fire"
+poke2:  .asciiz "water"
 sigual:  .asciiz "son iguales"
 noigual:  .asciiz "no iguales"
 .text
 
 main:	
-jal principal
+add $s2,$zero,$zero
+j principal
+#jal principal
 j end
 
+
 principal:
-	la $s0, matrizA
+	la $s0, Pokeskin
 	lw $s7, nroColumnas
 	add $t0, $zero, 0
-
-bucle1:
-	mul $t1, $t0, $s7
 	add $t6, $zero, 0
 bucle2:
 	addi $t2, $t1, 0
@@ -48,11 +48,14 @@ bucle2:
 	add $t3, $t2, $s0
 	lw $s3, ($t3)
 	add $t3, $s3, $zero
+
 	
 	move $a0, $t3
 	li $v0, 4
 	syscall
-	jal compare
+        jal compare
+        #beq $t5, $s7,principal
+        
 	li $v0, 4
 	la $a0, espacio
 	syscall
@@ -64,27 +67,32 @@ bucle2:
 	la $a0, salto
 	syscall
 
-	addi $t0, $t0, 1
+	#addi $t0, $t0, 1
 	jal end
 #	blt $t0, $s7, bucle1
 compare:
-	addi $sp, $sp, -20
+	addi $sp, $sp, -24
     	sw $a0,0($sp)
     	sw $a1,4($sp)
     	sw $ra,8($sp)
     	sw $t0,16($sp)
     	sw $t1,20($sp)
 	la $a0,($s3)
-	la $a1,poke#pokemon a comparar
 	li $v0, 0
-	jal loopc
+	beqz $s2,compoke
+	   la $a1,poke2#pokemon a comparar
+	   j comcontinua
+	compoke:
+	   la $a1,poke1 #pokemon a comparar
+	comcontinua:
+	j loopc
 i:
 	lw $a0,0($sp)
     	lw $a1,4($sp)
     	lw $ra,8($sp)
     	lw $t0,16($sp)
     	lw $t1,20($sp)
-        addi $sp, $sp, 20
+        addi $sp, $sp, 24
 	jr $ra
 loopc:
 	lb $t0($a0)
@@ -108,7 +116,10 @@ igual:
 	addi $t6,$t6,1 #indice de tipo
 	la $a0, ($t6)
 	syscall	
-	j end
+	
+	beq $s2,1,end
+	addi $s2,$zero,1 ##
+	j principal
 lend:
 	beq $t0, $t1, igual
 	beqz $t0, menor
